a601d36095881de60230e24144bf58f50f9d8733
Yunus Emre <me@yek.plus>
2022-09-18 09:30:56 +0200

    update firebase config import

--- next.config.js
+++ next.config.js
@@ -12,7 +12,7 @@ const nextConfig = {
     FIREBASE_type: process.env.FIREBASE_type,
     FIREBASE_project_id: process.env.FIREBASE_project_id,
     FIREBASE_private_key_id: process.env.FIREBASE_private_key_id,
-    FIREBASE_private_key: process.env.FIREBASE_private_key,
+    FIREBASE_private_key: process.env.FIREBASE_private_key.replace(/\\n/gm, "\n"),
     FIREBASE_client_email: process.env.FIREBASE_client_email,
     FIREBASE_client_id: process.env.FIREBASE_client_id,
     FIREBASE_auth_uri: process.env.FIREBASE_auth_uri,
