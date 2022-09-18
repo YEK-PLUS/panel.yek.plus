f72385a8dfef9ddefb56c58a3938e916d86dce0c
Yunus Emre Köker <yunusemrekokerr@gmail.com>
2022-09-18 05:08:22 +0200

    setup firebase config

--- next.config.js
+++ next.config.js
@@ -9,6 +9,16 @@ const nextConfig = {
   // define api side env variables
   serverRuntimeConfig: {
     JWT_SECRET: process.env.JWT_SECRET,
+    FIREBASE_type: process.env.FIREBASE_type,
+    FIREBASE_project_id: process.env.FIREBASE_project_id,
+    FIREBASE_private_key_id: process.env.FIREBASE_private_key_id,
+    FIREBASE_private_key: process.env.FIREBASE_private_key,
+    FIREBASE_client_email: process.env.FIREBASE_client_email,
+    FIREBASE_client_id: process.env.FIREBASE_client_id,
+    FIREBASE_auth_uri: process.env.FIREBASE_auth_uri,
+    FIREBASE_token_uri: process.env.FIREBASE_token_uri,
+    FIREBASE_auth_provider_x509_cert_url: process.env.FIREBASE_auth_provider_x509_cert_url,
+    FIREBASE_client_x509_cert_url: process.env.FIREBASE_client_x509_cert_url,
   },
 }
 
