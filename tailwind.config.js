32a256854f76af8c1c1150a3b7a5af19351e1386
Yunus Emre <me@yek.plus>
2022-09-29 15:51:25 +0200

    update tailwind and deps

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,5 +8,8 @@ module.exports = {
   theme: {
     extend: {},
   },
-  plugins: [],
-}
+  variants: {
+    scrollbar: ["rounded"],
+  },
+  plugins: [require("tailwind-scrollbar")({ nocompatible: true })],
+};
