a3dfe1e7a8174cfde7e1418c97f557727832bb71
Yunus Emre <me@yek.plus>
2022-09-29 15:47:36 +0200

    add new fetchs

--- api/fetcher.ts
+++ api/fetcher.ts
@@ -32,3 +32,39 @@ export const post: Fetcher = async (url, data) => {
   });
   return await res.json();
 };
+export const get: Fetcher = async (url, data) => {
+  const cookies = new Cookies();
+  const res = await fetch(API_URL + url, {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json",
+      Cookie: `token=${cookies.get("token")}`,
+    },
+    body: JSON.stringify(data),
+  });
+  return await res.json();
+};
+export const _delete: Fetcher = async (url, data) => {
+  const cookies = new Cookies();
+  const res = await fetch(API_URL + url, {
+    method: "DELETE",
+    headers: {
+      "Content-Type": "application/json",
+      Cookie: `token=${cookies.get("token")}`,
+    },
+    body: JSON.stringify(data),
+  });
+  return await res.json();
+};
+export const patch: Fetcher = async (url, data) => {
+  const cookies = new Cookies();
+  const res = await fetch(API_URL + url, {
+    method: "PATCH",
+    headers: {
+      "Content-Type": "application/json",
+      Cookie: `token=${cookies.get("token")}`,
+    },
+    body: JSON.stringify(data),
+  });
+  return await res.json();
+};
